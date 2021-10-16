c5c8fc15f5d9142b385d867e4f2634026e2a143e
Nkubito Pacis <pacisnkubito@gmail.com>
2021-10-16 10:32:23 +0200

    Fix: Fixing a bug in user registration

--- validators/manager.validator.js
+++ validators/manager.validator.js
@@ -11,8 +11,7 @@ exports.validateManager = async(req,res,next)=>{
         Email:Joi.string().min(5).required(),
         Password:Joi.string().min(6).required()
     })
-    const employee = manager;
-    const {error} = schema.validate(employee);   
+    const {error} = schema.validate(req.body);   
     if(error){
         return res.status(400).json({
             data: error.message,
@@ -20,24 +19,24 @@ exports.validateManager = async(req,res,next)=>{
         })
     }
      
-    let checkEmail = await User.findOne({ Email: manager.Email })
+    let checkEmail = await User.findOne({ Email: req.body.Email })
     if (checkEmail) return res.status(400).send("Email is already registered!")
     
-    let checkNationalID = await User.findOne({ NationalId: manager.NationalId })
+    let checkNationalID = await User.findOne({ NationalId: req.body.NationalId })
     if (checkNationalID) return res.status(400).send("National Id is already registered!")
     
-    let checkPhone = await User.findOne({ Phone: manager.Phone })
+    let checkPhone = await User.findOne({ Phone: req.body.Phone })
     if (checkPhone) return res.status(400).send("Phone Number is already registered!")
-    if((manager.Phone).length < 10 ) return res.status(400).send("Phone Number must be 10 characters!")
-    manager.Phone = (manager.Phone).toString()
+    if((req.body.Phone).length < 10 ) return res.status(400).send("Phone Number must be 10 characters!")
+    req.body.Phone = (req.body.Phone).toString()
     let validRwandanPhoneNumbers = ['078','079','072','073']
-    let first3Characters = (manager.Phone.toString()).substring(0,3)
+    let first3Characters = (req.body.Phone.toString()).substring(0,3)
     if(validRwandanPhoneNumbers.includes(first3Characters) != true){
         return res.status(400).send("Phone Number must be a valid Rwandan Phone Number!")
     }
     
-    if(manager.DateOfBirth){
-        let date = new Date(manager.DateOfBirth).getFullYear()
+    if(req.body.DateOfBirth){
+        let date = new Date(req.body.DateOfBirth).getFullYear()
         let today = new Date()
         if((today.getFullYear() - date) < 18){
             return res.status(400).send("You are not eligible to register because the provided age is less than 18 years")
